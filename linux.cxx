16dfdecb8ecaf991b44bc11522f510f22fcc7801
Yakov Savelev (Phone) <g6h6m238929@gmail.com>
2023-09-06 09:29:28 +0500

    Version 0.10

--- linux.cxx
+++ linux.cxx
@@ -21,7 +21,31 @@ vector<string> split(string lines, string splitter) {
 }
 
 string get_cl_answer(string command) {
-	return "";
+	FILE* pipe = popen(command.c_str(), "r");
+    if (!pipe) {
+        cerr << "Unknow error!" << endl;
+        return -1;
+    }
+    
+    char buffer[128];
+    string result = "";
+    
+    while (fgets(buffer, 128, pipe) != nullptr) {
+        result += buffer;
+    }
+    
+    pclose(pipe);
+
+    return result;
+}
+
+vector<vector<string>> maping(vector<string> lines_splitted) {
+	vector<vector<string>> lines_double_splitted;
+	for (string line : lines_splitted) {
+		lines_double_splitted.push_back(split(line, " "));
+	}
+	
+	return lines_double_splitted;
 }
 
 vector<string> get_mount_point() {
